843655d18bb78849eaa623933129bbf91d30a9d4
Halgand Ervin <halgand.ervin@gmail.com>
2021-11-07 23:51:35 +0100

    test page route console log

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -11,7 +11,7 @@ const Header = () => {
     const navRef = useRef();
     const [isOpen, setIsOpen] = useState(false);
     const { width, height } = useWindowDimensions();
-
+    console.log(router);
     useEffect(() => {
         if (!navRef || !width) return;
         Array.from(navRef.current.children).forEach(element => {
